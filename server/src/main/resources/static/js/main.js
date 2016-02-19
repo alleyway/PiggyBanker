f7c34709c48b6d8c5251673551eff1d7cb2a57c3
Michael Lake <mlake900@gmail.com>
2016-02-18 20:18:39 -0500

    animating coin

--- server/src/main/resources/static/js/main.js
+++ server/src/main/resources/static/js/main.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
+$(function () {
     var container = $("#barcode_container");
+
+    var coinOrigWidth = $("#coin").width();
 
     // defined a connection to a new socket endpoint
     var socket = new SockJS('/stomp');
@@ -12,9 +14,54 @@ $(document).ready(function () {
             var message = JSON.parse(data.body);
             container.empty();
             $("<img/>", {
-                src: "data:image/svg+xml;base64," + btoa(message.barcodeContent),
-                class: "img-responsive"
+                src: "data:image/svg+xml;base64," + btoa(message.barcodeContent)
+
             }).appendTo(container);
+            animateIn();
         });
     });
+
+
+
+    function animateIn() {
+        var coin = $("#coin");
+        var left = -.85 * $(window).width();
+
+        console.log("left: " + left);
+        coin.css({left: left, opacity: 0.25, rotation: 0}).animate({"left": "0px", opacity: 1.0, rotation: 360}, {
+            duration: 700,
+            easing: "easeInOutQuad",
+            step: function (angle, fx) {
+                if (fx.prop != "rotation") return;
+                //console.log("angle: " + angle);
+                $(this).css({
+                    "-moz-transform": "rotate(" + angle + "deg)",
+                    "-webkit-transform": "rotate(" + angle + "deg)",
+                    "-ms-transform": "rotate(" + angle + "deg)",
+                    "transform": "rotate(" + angle + "deg)"
+                });
+            },
+            complete: function() {
+                animateOut();
+            }
+        });
+    }
+
+    function animateOut() {
+        var coin = $("#coin");
+        var top = 0;
+
+        //ratio of pig hole to pig image width
+        var slotWidth = (150/785) * $("#piggy_bank").width() * .9;
+
+
+        coin.css({top: top, width: coinOrigWidth}).animate({"top": "250px", width: slotWidth}, {
+            duration: 700,
+            easing: "easeInOutExpo",
+            complete: function() {
+                $(this).css( {opacity: 0.0, top: 0, width: coinOrigWidth});
+            }
+        });
+    }
 });
+
