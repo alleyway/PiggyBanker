9bf9893c17707800fdc450fd12b2ffdc58d62661
Michael Lake <mlake900@gmail.com>
2016-02-21 12:42:06 -0500

    Design updates

--- app/src/main/java/com/alleywayconsulting/piggygraph/MainActivity.java
+++ app/src/main/java/com/alleywayconsulting/piggygraph/MainActivity.java
@@ -1,19 +1,18 @@
 package com.alleywayconsulting.piggygraph;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import com.alleywayconsulting.piggygraph.zxing.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void scanCodes(View view) {
